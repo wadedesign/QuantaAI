dd67e04c623a6e5e337b052c0519041ec65828b2
wadder12 <awade75009@gmail.com>
2023-05-30 06:59:33 +0000

    May 30, 2023, 1:59 AM

--- commands/cogs/testing/dev1.py
+++ commands/cogs/testing/dev1.py
@@ -221,6 +221,40 @@ class Developer1(commands.Cog):
                 color=nextcord.Color.red()
             )
             await interaction.response.send_message(embed=error_embed, ephemeral=True)
+            
+    @dev2.subcommand(name="housestock", description="Fetch house stock data")
+    async def housestock(self, interaction: nextcord.Interaction):
+        try:
+            url = "https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/all_transactions.json"
+
+            response = requests.get(url)
+            response.raise_for_status()  # Check for any HTTP errors
+
+            data = response.json()
+
+            # Create an embed to display the data
+            embed = nextcord.Embed(title="House Stock Data", color=nextcord.Color.green())
+
+            # Add fields to the embed for each transaction
+            for transaction in data:
+                house_id = transaction["house_id"]
+                stock_symbol = transaction["stock_symbol"]
+                quantity = transaction["quantity"]
+                price = transaction["price"]
+
+                field_value = f"House ID: {house_id}\nStock Symbol: {stock_symbol}\nQuantity: {quantity}\nPrice: {price}"
+                embed.add_field(name="Transaction", value=field_value, inline=False)
+
+            await interaction.response.send_message(embed=embed, ephemeral=True)
+
+        except Exception as e:
+            print(str(e))
+            error_embed = nextcord.Embed(
+                title="Error Occurred",
+                description="An error occurred while fetching the house stock data.",
+                color=nextcord.Color.red()
+            )
+            await interaction.response.send_message(embed=error_embed, ephemeral=True)
 
 def setup(bot):
     bot.add_cog(Developer1(bot))
