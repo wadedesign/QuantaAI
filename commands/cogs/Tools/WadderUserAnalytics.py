96a29ee43aeb44505ed19da2c26015f323da7c55
wadder12 <awade75009@gmail.com>
2023-05-31 03:04:19 +0000

    May 30, 2023, 10:04 PM

--- commands/cogs/Tools/WadderUserAnalytics.py
+++ commands/cogs/Tools/WadderUserAnalytics.py
@@ -1,5 +1,7 @@
 import nextcord
 from nextcord.ext import commands
+from nextcord import File
+import os
 
 class UserAnalyticsCog(commands.Cog):
     def __init__(self, bot):
@@ -36,14 +38,34 @@ class UserAnalyticsCog(commands.Cog):
             message_count = self.message_count.get(guild_id, {}).get(user.id, 0)
             message_count_embed = nextcord.Embed(title=f"Message Count for {user.display_name}")
             message_count_embed.add_field(name="Message Count", value=f"{message_count} \U0001F4AC")  # Emoji: 💬
-            await ctx.send(embed=message_count_embed)
+
+            # Get the path to the GIF file in your project's directory
+            gif_path = os.path.join(os.getcwd(), 'quanta.gif')
+
+            # Load and attach the GIF file
+            with open(gif_path, 'rb') as gif_file:
+                gif = File(gif_file, filename='animated.gif')
+                message_count_embed.set_image(url="attachment://animated.gif")
+
+            await ctx.send(file=gif, embed=message_count_embed)
         else:
             message_count_embed = nextcord.Embed(title="Message Count")
             for user_id, count in self.message_count.get(guild_id, {}).items():
                 user = self.bot.get_user(user_id)
                 if user:
                     message_count_embed.add_field(name=user.display_name, value=f"{count} \U0001F4AC")  # Emoji: 💬
-            await ctx.send(embed=message_count_embed)
+
+            # Get the path to the GIF file in your project's directory
+            gif_path = os.path.join(os.getcwd(), 'quanta.gif')
+
+            # Load and attach the GIF file
+            with open(gif_path, 'rb') as gif_file:
+                gif = File(gif_file, filename='animated.gif')
+                message_count_embed.set_image(url="attachment://animated.gif")
+
+            await ctx.send(file=gif, embed=message_count_embed)
 
 def setup(bot):
     bot.add_cog(UserAnalyticsCog(bot))
+
+
