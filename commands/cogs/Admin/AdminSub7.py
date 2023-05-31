c741b29b7ef81d6a07855fae1813babbed6fcacf
wadder12 <awade75009@gmail.com>
2023-05-31 06:00:47 +0000

    May 31, 2023, 1:00 AM

--- commands/cogs/Admin/AdminSub7.py
+++ commands/cogs/Admin/AdminSub7.py
@@ -8,22 +8,17 @@ import json
 import time
 import os
 
-## Todo: Add a command to make the AFK go away
-## Todo: Make more sub commands of this, not utilizing the space well enough. 
-
 class AwayFromKeyboard(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.afk_file = 'data/afk.json'
         self.create_afk_file()
         
-    
     def create_afk_file(self):
         os.makedirs(os.path.dirname(self.afk_file), exist_ok=True)
         if not os.path.exists(self.afk_file):
             with open(self.afk_file, 'w') as f:
                 json.dump({}, f)
-    
     
     async def update_data(self, afk, user):
         if not f'{user.id}' in afk:
@@ -58,7 +53,7 @@ class AwayFromKeyboard(commands.Cog):
                 reason = afk[user_id]['reason']
                 meth = int(time.time()) - int(afk[user_id]['time'])
                 been_afk_for = await self.time_formatter(meth)
-                embed = nextcord.Embed(description=f'{user_mention.name} Is currently AFK!\nReason : {reason}')
+                embed = nextcord.Embed(description=f'{user_mention.name} is currently AFK!\nReason: {reason}')
                 await message.channel.send(content=message.author.mention, embed=embed)
 
                 meeeth = int(afk[user_id]['mentions']) + 1
@@ -70,14 +65,13 @@ class AwayFromKeyboard(commands.Cog):
             await self.update_data(afk, message.author)
 
             if afk[f'{message.author.id}']['AFK'] == 'True':
-                
                 meth = int(time.time()) - int(afk[f'{message.author.id}']['time'])
                 been_afk_for = await self.time_formatter(meth)
                 mentionz = afk[f'{message.author.id}']['mentions']
 
-                embed = nextcord.Embed(description=f'Welcome Back {message.author.name}!', color=0x00ff00)
-                embed.add_field(name="You've been AFK for :", value=been_afk_for, inline=False)
-                embed.add_field(name='Times you got mentioned while you were afk :', value=mentionz, inline=False)
+                embed = nextcord.Embed(description=f'Welcome back {message.author.name}!', color=0x00ff00)
+                embed.add_field(name="You've been AFK for:", value=been_afk_for, inline=False)
+                embed.add_field(name='Times you were mentioned while AFK:', value=mentionz, inline=False)
 
                 await message.channel.send(content=message.author.mention, embed=embed)
                 
@@ -92,13 +86,13 @@ class AwayFromKeyboard(commands.Cog):
                 try:
                     await message.author.edit(nick=f'{message.author.display_name[5:]}')
                 except:
-                    print(f'I wasnt able to edit [{message.author}].')
+                    print(f'I was not able to edit [{message.author}].')
         
         with open('data/afk.json', 'w') as f:
             json.dump(afk, f)
 
-    @nextcord.slash_command(name = 'afk',description='Set Your AFK to let others know when they ping you')
-    async def afk_a(self,interac : Interaction,reason):
+    @nextcord.slash_command(name='afk', description='Set your AFK to let others know when they ping you')
+    async def afk(self, interac: Interaction, reason):
         with open('data/afk.json', 'r') as f:
             afk = json.load(f)
 
@@ -111,18 +105,41 @@ class AwayFromKeyboard(commands.Cog):
         afk[f'{interac.user.id}']['time'] = int(time.time())
         afk[f'{interac.user.id}']['mentions'] = 0
 
-        embed = nextcord.Embed(description=f"I've set your AFK {interac.user.display_name}!\nReason : {reason}", color=0x00ff00)
+        embed = nextcord.Embed(description=f"I've set your AFK, {interac.user.display_name}!\nReason: {reason}", color=0x00ff00)
         await interac.response.send_message(content=interac.user.mention, embed=embed)
 
         with open('data/afk.json', 'w') as f:
             json.dump(afk, f)
+        
         try:
             await interac.user.edit(nick=f'[AFK]{interac.user.display_name}')
         except:
-            print(f'I wasnt able to edit [{interac.user}].')        
+            print(f'I was not able to edit [{interac.user}].')
+    
+    @nextcord.slash_command(name='removeafk', description='Remove your AFK status')
+    async def remove_afk(self, interac: Interaction):
+        with open('data/afk.json', 'r') as f:
+            afk = json.load(f)
+        
+        await self.update_data(afk, interac.user)
+        if afk[f'{interac.user.id}']['AFK'] == 'True':
+            afk[f'{interac.user.id}']['AFK'] = 'False'
+            afk[f'{interac.user.id}']['reason'] = 'None'
+            afk[f'{interac.user.id}']['time'] = '0'
+            afk[f'{interac.user.id}']['mentions'] = 0
+            
+            with open('data/afk.json', 'w') as f:
+                json.dump(afk, f)
+            
+            try:
+                await interac.user.edit(nick=f'{interac.user.display_name[5:]}')
+            except:
+                print(f'I was not able to edit [{interac.user}].')
+            
+            await interac.response.send_message(content=interac.user.mention, content="Your AFK status has been removed.")
+        else:
+            await interac.response.send_message(content=interac.user.mention, content="You are not currently AFK.")
 
-        
-        
 def setup(bot):
     bot.add_cog(AwayFromKeyboard(bot))
     print('AFK Cog is loaded')
