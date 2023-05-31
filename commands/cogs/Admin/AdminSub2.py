698851a777821a7f68e9bca9d36c87436641fc0c
wadder12 <awade75009@gmail.com>
2023-05-31 01:34:39 +0000

    May 30, 2023, 8:34 PM

--- commands/cogs/Admin/AdminSub2.py
+++ commands/cogs/Admin/AdminSub2.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 import random
 
 import nextcord
@@ -9,7 +10,8 @@ import asyncpraw
 from nextcord import Embed
 from nextcord.ext.commands import Context
 from datetime import datetime
-
+import plotly.graph_objects as go
+import os 
 
 
 class WadderCommandsV1(commands.Cog):
@@ -323,8 +325,47 @@ class WadderCommandsV1(commands.Cog):
         await interaction.send(embed=embed)
     
     
+    @main.subcommand(name="messagestats", description="Get message statistics for the last 100 messages")
+    async def message_stats(self, interaction: nextcord.Interaction):
+        await interaction.response.defer()
+        messages = await interaction.channel.history(limit=100).flatten()  # Fetches the latest 100 messages
 
+        users = {}
+        for message in messages:
+            if message.author.bot:  # Skip messages sent by bots
+                continue
 
+            if message.author.id not in users:
+                users[message.author.id] = 1
+            else:
+                users[message.author.id] += 1
+
+        usernames = []
+        message_counts = []
+        for user_id, count in users.items():
+            user = await self.bot.fetch_user(user_id)
+            usernames.append(user.name)
+            message_counts.append(count)
+
+        # Create a bar chart using Plotly
+        fig = go.Figure(data=go.Bar(x=usernames, y=message_counts))
+        fig.update_layout(
+            title="Message Statistics",
+            xaxis_title="User",
+            yaxis_title="Number of Messages",
+        )
+
+        # Save the chart as an image
+        chart_filename = "message_stats.png"
+        fig.write_image(chart_filename)
+
+        # Send the chart as an image attachment
+        with open(chart_filename, "rb") as file:
+            chart_image = nextcord.File(file)
+            await interaction.send(file=chart_image)
+
+        # Remove the chart image file
+        os.remove(chart_filename)
        
 
             
