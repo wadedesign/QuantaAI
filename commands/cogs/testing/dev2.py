2515f13c25fda3630b2b3cbb2c1d0a06b1d8f2ac
wadder12 <awade75009@gmail.com>
2023-06-02 05:34:50 +0000

    Jun 2, 2023, 12:34 AM

---
+++ commands/cogs/testing/dev2.py
@@ -0,0 +1,50 @@
+import nextcord
+from nextcord.ext import commands
+import requests
+
+
+class Developer2(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+        
+        
+    @nextcord.slash_command(name='dev3')
+    async def dev3(self, interaction: nextcord.Interaction):
+       pass
+   
+   
+    @dev3.subcommand(name="sales_tax",description="Get the sales tax rate for a specific location")
+    async def sales_tax(self, interaction: nextcord.Interaction, city: str, state: str, street: str, zip_code: str):
+        url = "https://sales-tax-calculator.p.rapidapi.com/rates"
+        payload = {
+            "city": city,
+            "state": state,
+            "street": street,
+            "zip": zip_code
+        }
+        headers = {
+            "content-type": "application/json",
+            "X-RapidAPI-Key": "82cfc7318cmsh3f3e03fa5eb7fdfp16eb9cjsn5bd4ea35cd19",
+            "X-RapidAPI-Host": "sales-tax-calculator.p.rapidapi.com"
+        }
+
+        response = requests.post(url, json=payload, headers=headers)
+        tax_rate = response.json().get("rate", 0)
+
+        await interaction.response.send_message(f"The sales tax rate for {city}, {state}, {zip_code} is {tax_rate}%.", ephemeral=True)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+def setup(bot):
+    bot.add_cog(Developer2(bot))
