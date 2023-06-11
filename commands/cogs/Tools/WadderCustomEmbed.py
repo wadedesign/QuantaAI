4b197ce616211a871cd611530127f2f5b05ccf4a
wadder12 <awade75009@gmail.com>
2023-06-11 08:00:22 +0000

    Jun 11, 2023, 3:00 AM

--- commands/cogs/Tools/WadderCustomEmbed.py
+++ commands/cogs/Tools/WadderCustomEmbed.py
@@ -1,26 +1,29 @@
 import nextcord
 from nextcord.ext import commands
-
+from nextcord.ui import TextInput, VBox
 
 class EmbedCus(nextcord.ui.Modal):
     def __init__(self):
-        super().__init__(
-            "Embed Maker",
-        )
-        self.emTitle = nextcord.ui.TextInput(label="Embed Title", min_length=2, max_length=124, required=True, placeholder="Enter the embed title here")
-        self.add_item(self.emTitle)
-        self.emDesc = nextcord.ui.TextInput(label="Embed Description", min_length=5, max_length=4000, required=True, placeholder="Enter the embed description here", style=nextcord.TextInputStyle.paragraph)
-        self.add_item(self.emDesc)
-        self.channelID = nextcord.ui.TextInput(label="Channel ID", required=True, placeholder="Enter the channel ID")
-        self.add_item(self.channelID)
-        self.footerText = nextcord.ui.TextInput(label="Footer Text", min_length=1, max_length=2048, required=False, placeholder="Enter the footer text")
-        self.add_item(self.footerText)
-        self.timestamp = nextcord.ui.TextInput(label="Timestamp", required=False, placeholder="Enter the timestamp")
-        self.add_item(self.timestamp)
-        self.footerImage = nextcord.ui.TextInput(label="Footer Image URL", required=False, placeholder="Enter the URL of the footer image")
-        self.add_item(self.footerImage)
+        super().__init__("Embed Maker")
+        
+        self.emTitle = TextInput(label="Embed Title", min_length=2, max_length=124, required=True, placeholder="Enter the embed title here")
+        self.emDesc = TextInput(label="Embed Description", min_length=5, max_length=4000, required=True, placeholder="Enter the embed description here", style=nextcord.TextInputStyle.paragraph)
+        self.channelID = TextInput(label="Channel ID", required=True, placeholder="Enter the channel ID")
+        self.footerText = TextInput(label="Footer Text", min_length=1, max_length=2048, required=False, placeholder="Enter the footer text")
+        self.timestamp = TextInput(label="Timestamp", required=False, placeholder="Enter the timestamp")
+        self.footerImage = TextInput(label="Footer Image URL", required=False, placeholder="Enter the URL of the footer image")
         self.color = nextcord.ui.ColorInput(label="Color", required=False, default=nextcord.Color.blurple())
-        self.add_item(self.color)
+        
+        vbox = VBox()
+        vbox.add_item(self.emTitle)
+        vbox.add_item(self.emDesc)
+        vbox.add_item(self.channelID)
+        vbox.add_item(self.footerText)
+        vbox.add_item(self.timestamp)
+        vbox.add_item(self.footerImage)
+        vbox.add_item(self.color)
+        
+        self.add_item(vbox)
 
     async def callback(self, interaction: nextcord.Interaction) -> None:
         title = self.emTitle.value
@@ -53,10 +56,11 @@ class UserMod(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @nextcord.slash_command(name="cusembed", description="Create an embed")
+    @commands.slash_command(name="cusembed", description="Create an embed")
     async def embed(self, interaction: nextcord.Interaction):
         await interaction.response.send_modal(EmbedCus())
 
 def setup(bot):
     bot.add_cog(UserMod(bot))
+
 
