913665a52d0522c37214863958e15d4b0f82230b
wadder12 <awade75009@gmail.com>
2023-06-11 19:51:42 +0000

    Jun 11, 2023, 2:51 PM

--- commands/cogs/Server/WadderAntiSpam.py
+++ commands/cogs/Server/WadderAntiSpam.py
@@ -1,5 +1,6 @@
 import time
 from collections import defaultdict
+from nextcord import Embed
 from nextcord.ext import commands
 
 class AntiSpam(commands.Cog):
@@ -23,10 +24,17 @@ class AntiSpam(commands.Cog):
             self.user_message_count[user_id] += 1
             if self.user_message_count[user_id] > self.message_limit:
                 await message.delete()
-                await message.channel.send(f"{message.author.mention}, please stop spamming.")
+                await message.channel.send(embed=self.create_spam_embed(message.author))
         else:
             self.user_message_count[user_id] = 1
         self.user_last_message_time[user_id] = current_time
 
+    def create_spam_embed(self, author):
+        embed = Embed(title="Anti-Spam", color=0xff0000)
+        embed.description = f"{author.mention}, please stop spamming."
+        embed.set_thumbnail(url=author.avatar_url)
+        return embed
+
 def setup(bot):
     bot.add_cog(AntiSpam(bot))
+
