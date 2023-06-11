47104b69d3971dc6b194d4811a30f9bf936f9d19
wadder12 <awade75009@gmail.com>
2023-06-11 19:48:51 +0000

    Jun 11, 2023, 2:48 PM

--- commands/cogs/Server/WadderAntiSpam.py
+++ commands/cogs/Server/WadderAntiSpam.py
@@ -19,14 +19,14 @@ class AntiSpam(commands.Cog):
         user_id = message.author.id
 
         # Check if the user has exceeded the message limit
-        if (current_time - self.user_last_message_time[user_id]) < self.time_window:
+        if current_time - self.user_last_message_time[user_id] < self.time_window:
             self.user_message_count[user_id] += 1
             if self.user_message_count[user_id] > self.message_limit:
                 await message.delete()
                 await message.channel.send(f"{message.author.mention}, please stop spamming.")
         else:
             self.user_message_count[user_id] = 1
-            self.user_last_message_time[user_id] = current_time
+        self.user_last_message_time[user_id] = current_time
 
 def setup(bot):
     bot.add_cog(AntiSpam(bot))
