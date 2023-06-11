8167dcc4f7d1b7f9b6d5b75898b806016c70306d
wadder12 <awade75009@gmail.com>
2023-06-11 19:56:17 +0000

    Jun 11, 2023, 2:56 PM

--- commands/cogs/Server/WadderAntiSpam.py
+++ commands/cogs/Server/WadderAntiSpam.py
@@ -10,6 +10,8 @@ class AntiSpam(commands.Cog):
         self.user_last_message_time = defaultdict(float)
         self.message_limit = 5  # Adjust the limit of messages allowed in the time window
         self.time_window = 10  # Adjust the time window in seconds
+        self.command_message_count = defaultdict(int)
+        self.command_message_limit = 3  # Adjust the limit of command messages allowed in the time window
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -29,6 +31,13 @@ class AntiSpam(commands.Cog):
             self.user_message_count[user_id] = 1
         self.user_last_message_time[user_id] = current_time
 
+        # Check if the user has exceeded the command message limit
+        if message.content.startswith(self.bot.command_prefix):
+            self.command_message_count[user_id] += 1
+            if self.command_message_count[user_id] > self.command_message_limit:
+                await message.delete()
+                await message.channel.send(embed=self.create_spam_embed(message.author))
+
     def create_spam_embed(self, author):
         embed = Embed(title="QAnti-Spam", color=0xff0000)
         embed.description = f"{author.mention}, please stop spamming."
@@ -37,4 +46,5 @@ class AntiSpam(commands.Cog):
 
 def setup(bot):
     bot.add_cog(AntiSpam(bot))
+
 
