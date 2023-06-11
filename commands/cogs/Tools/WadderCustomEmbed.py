8d11098c6dbeb04d0aa4bfb0558c5cf16f52ac85
wadder12 <awade75009@gmail.com>
2023-06-11 08:14:08 +0000

    Jun 11, 2023, 3:14 AM

--- commands/cogs/Tools/WadderCustomEmbed.py
+++ commands/cogs/Tools/WadderCustomEmbed.py
@@ -1,6 +1,7 @@
 import nextcord
 from nextcord.ext import commands
 
+# ** Ready for Production **
 
 class EmbedCus(nextcord.ui.Modal):
     def __init__(self):
@@ -9,20 +10,20 @@ class EmbedCus(nextcord.ui.Modal):
         )
         self.emTitle = nextcord.ui.TextInput(label="Embed Title", min_length=2, max_length=124, required=True, placeholder="Enter the embed title here")
         self.add_item(self.emTitle)
-        self.emDesc = nextcord.ui.TextInput(label="Embed Description", min_length=5, max_length=4000, required=True, placeholder="Enter the embed description here", style=nextcord.TextInputStyle.paragraph)
+        self.emDesc = nextcord.ui.TextInput(label="Embed Description", min_length=5, max_length=2048, required=True, placeholder="Enter the embed description here", style=nextcord.TextInputStyle.paragraph)
         self.add_item(self.emDesc)
         self.channelID = nextcord.ui.TextInput(label="Channel ID", required=True, placeholder="Enter the channel ID")
         self.add_item(self.channelID)
-        self.footerText = nextcord.ui.TextInput(label="Footer Text", min_length=1, max_length=2048, required=False, placeholder="Enter the footer text")
-        self.add_item(self.footerText)
-        self.footerImage = nextcord.ui.TextInput(label="Footer Image URL", required=False, placeholder="Enter the URL of the footer image")
-        self.add_item(self.footerImage)
-        self.color = nextcord.ui.Select(options=[
-            nextcord.Color.red(),
-            nextcord.Color.green(),
-            nextcord.Color.blue()
-        ])
-        self.add_item(self.color)
+        # self.footerText = nextcord.ui.TextInput(label="Footer Text", min_length=1, max_length=2048, required=False, placeholder="Enter the footer text")
+        # self.add_item(self.footerText)
+        # self.footerImage = nextcord.ui.TextInput(label="Footer Image URL", required=False, placeholder="Enter the URL of the footer image")
+        # self.add_item(self.footerImage)
+        # self.color = nextcord.ui.Select(options=[
+        #     nextcord.Color.red(),
+        #     nextcord.Color.green(),
+        #     nextcord.Color.blue()
+        # ], required=False, default=nextcord.Color.blurple())
+        # self.add_item(self.color)
 
     async def callback(self, interaction: nextcord.Interaction) -> None:
         title = self.emTitle.value
@@ -30,13 +31,13 @@ class EmbedCus(nextcord.ui.Modal):
         channel_id = int(self.channelID.value) if self.channelID.value is not None else None
         em = nextcord.Embed(title=title, description=desc)
 
-        footer_text = self.footerText.value
-        if footer_text:
-            em.set_footer(text=footer_text)
+        # footer_text = self.footerText.value
+        # if footer_text:
+        #     em.set_footer(text=footer_text)
 
-        footer_image_url = self.footerImage.value
-        if footer_image_url:
-            em.set_footer(icon_url=footer_image_url)
+        # footer_image_url = self.footerImage.value
+        # if footer_image_url:
+        #     em.set_footer(icon_url=footer_image_url)
 
         em.color = self.color.value
 
