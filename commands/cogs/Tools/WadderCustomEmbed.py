ef78f65f8bd231b26524f964cd11ee19b770fc79
wadder12 <awade75009@gmail.com>
2023-06-11 07:59:42 +0000

    Jun 11, 2023, 2:59 AM

--- commands/cogs/Tools/WadderCustomEmbed.py
+++ commands/cogs/Tools/WadderCustomEmbed.py
@@ -15,10 +15,7 @@ class EmbedCus(nextcord.ui.Modal):
         self.add_item(self.channelID)
         self.footerText = nextcord.ui.TextInput(label="Footer Text", min_length=1, max_length=2048, required=False, placeholder="Enter the footer text")
         self.add_item(self.footerText)
-        self.timestamp = nextcord.ui.Select(label="Timestamp", options=[
-            nextcord.SelectOption(label="Enabled", value="enabled"),
-            nextcord.SelectOption(label="Disabled", value="disabled")
-        ], required=False)
+        self.timestamp = nextcord.ui.TextInput(label="Timestamp", required=False, placeholder="Enter the timestamp")
         self.add_item(self.timestamp)
         self.footerImage = nextcord.ui.TextInput(label="Footer Image URL", required=False, placeholder="Enter the URL of the footer image")
         self.add_item(self.footerImage)
@@ -30,21 +27,21 @@ class EmbedCus(nextcord.ui.Modal):
         desc = self.emDesc.value
         channel_id = int(self.channelID.value) if self.channelID.value is not None else None
         em = nextcord.Embed(title=title, description=desc)
-        
+
         footer_text = self.footerText.value
         if footer_text:
             em.set_footer(text=footer_text)
-        
-        timestamp_option = self.timestamp.values[0]
-        if timestamp_option == "enabled":
-            em.timestamp = nextcord.utils.utcnow()
-        
+
+        timestamp = self.timestamp.value
+        if timestamp:
+            em.timestamp = timestamp
+
         footer_image_url = self.footerImage.value
         if footer_image_url:
             em.set_footer(icon_url=footer_image_url)
-        
+
         em.color = self.color.value
-        
+
         target_channel = interaction.guild.get_channel(channel_id)
         if target_channel:
             await target_channel.send(embed=em)
