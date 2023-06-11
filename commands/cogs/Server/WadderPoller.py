14ebe245e0b1262b5756f289e1aa45ff1020295e
wadder12 <awade75009@gmail.com>
2023-06-11 15:40:56 +0000

    Jun 11, 2023, 10:40 AM

--- commands/cogs/Server/WadderPoller.py
+++ commands/cogs/Server/WadderPoller.py
@@ -21,13 +21,11 @@ class Poll(commands.Cog):
 
     @nextcord.slash_command(name="cpoll",description="Create a poll with up to 10 choices.")
     async def cpoll(self, interaction: nextcord.Interaction, question: str):
-        if len(choices) > len(self.vote_emojis):
+        if len(interaction.options) > len(self.vote_emojis):
             await interaction.send("You can have a maximum of 10 choices.")
             return
 
-        choices = []
-        for choice in interaction.options:
-            choices.append(choice.value)
+        choices = [choice.value for choice in interaction.options]
 
         embed = nextcord.Embed(title=question, description="React with the corresponding emoji to vote.")
         for idx, choice in enumerate(choices):
@@ -59,5 +57,6 @@ class Poll(commands.Cog):
 
 def setup(bot):
     bot.add_cog(Poll(bot))
+
 
 
