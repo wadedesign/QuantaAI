d265ef2e776e3be89472247f3c06707a5a0e4c8c
wadder12 <awade75009@gmail.com>
2023-06-11 07:51:44 +0000

    Jun 11, 2023, 2:51 AM

--- commands/cogs/Tools/WadderCustomEmbed.py
+++ commands/cogs/Tools/WadderCustomEmbed.py
@@ -2,8 +2,6 @@ import nextcord
 from nextcord.ext import commands
 
 
-# * Works well 
-
 class EmbedCus(nextcord.ui.Modal):
     def __init__(self):
         super().__init__(
@@ -15,12 +13,34 @@ class EmbedCus(nextcord.ui.Modal):
         self.add_item(self.emDesc)
         self.channelID = nextcord.ui.TextInput(label="Channel ID", required=True, placeholder="Enter the channel ID")
         self.add_item(self.channelID)
+        self.footerText = nextcord.ui.TextInput(label="Footer Text", min_length=1, max_length=2048, required=False, placeholder="Enter the footer text")
+        self.add_item(self.footerText)
+        self.timestamp = nextcord.ui.Checkbox(label="Timestamp", required=False)
+        self.add_item(self.timestamp)
+        self.footerImage = nextcord.ui.TextInput(label="Footer Image URL", required=False, placeholder="Enter the URL of the footer image")
+        self.add_item(self.footerImage)
+        self.color = nextcord.ui.ColorInput(label="Color", required=False, default=nextcord.Color.blurple())
+        self.add_item(self.color)
 
     async def callback(self, interaction: nextcord.Interaction) -> None:
         title = self.emTitle.value
         desc = self.emDesc.value
         channel_id = int(self.channelID.value) if self.channelID.value is not None else None
         em = nextcord.Embed(title=title, description=desc)
+        
+        footer_text = self.footerText.value
+        if footer_text:
+            em.set_footer(text=footer_text)
+        
+        if self.timestamp.checked:
+            em.timestamp = nextcord.utils.utcnow()
+        
+        footer_image_url = self.footerImage.value
+        if footer_image_url:
+            em.set_footer(icon_url=footer_image_url)
+        
+        em.color = self.color.value
+        
         target_channel = interaction.guild.get_channel(channel_id)
         if target_channel:
             await target_channel.send(embed=em)
@@ -38,4 +58,3 @@ class UserMod(commands.Cog):
 
 def setup(bot):
     bot.add_cog(UserMod(bot))
-
