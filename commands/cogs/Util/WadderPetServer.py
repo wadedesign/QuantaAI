04ae60b7351a99cb72596502edc16d99719c5850
wadder12 <awade75009@gmail.com>
2023-06-11 07:24:16 +0000

    Jun 11, 2023, 2:24 AM

--- commands/cogs/Util/WadderPetServer.py
+++ commands/cogs/Util/WadderPetServer.py
@@ -44,35 +44,39 @@ class PetCog(commands.Cog): # good, change to where it shows the user and use em
         print(self.bot.user.id)
         print(f'Ping: {round(self.bot.latency * 1000)}ms')
 
-    @commands.command()
-    async def feed(self, ctx):
+    @nextcord.slash_command(name="pet")
+    async def main(self, interaction: nextcord.Interaction):
+        pass
+
+    @main.subcommand()
+    async def feed(self, interaction: nextcord.Interaction):
         self.hunger += 10
         self.happiness += 2
         if self.hunger >= 100:
             self.hunger = 100
         if self.happiness >= 100:
             self.happiness = 100
-        await ctx.send(f'Nom nom')
+        await interaction.send(f'Nom nom')
         print("Fed. Current hunger: " + self.hunger)
 
-    @commands.command()
-    async def snuggle(self, ctx):
+    @main.subcommand()
+    async def snuggle(self, interaction: nextcord.Interaction):
         self.happiness += 10
         if self.happiness >= 100:
             self.happiness = 100
-        await ctx.send(f'Zzzz')
+        await interaction.send(f'Zzzz')
         print("Snuggled. Current happiness: " + self.happiness)
 
-    @commands.command()
-    async def pet(self, ctx):
+    @main.subcommand()
+    async def pet(self, interaction: nextcord.Interaction):
         self.happiness += 10
         if self.happiness >= 100:
             self.happiness = 100
-        await ctx.send(f'mew')
+        await interaction.send(f'mew')
         print("Petted. Current happiness: " + self.happiness)
 
-    @commands.command()
-    async def sleep(self, ctx):
+    @main.subcommand()
+    async def sleep(self, interaction: nextcord.Interaction):
         print(self.currstatus)
         if self.currstatus == "awake":
             self.happiness += 5
@@ -85,27 +89,27 @@ class PetCog(commands.Cog): # good, change to where it shows the user and use em
             print("now sleeping")
         else:
             print("already sleeping")
-            await ctx.send("meow??")
+            await interaction.send("meow??")
 
-    @commands.command()
-    async def wakeup(self, ctx):
+    @main.subcommand()
+    async def wakeup(self, interaction: nextcord.Interaction):
         if self.currstatus == "sleeping":
             self.health += 5
             if self.health >= 100:
                 self.health = 100
             self.currstatus = "awake"
         else:
-            await ctx.send("meow??")
+            await interaction.send("meow??")
 
 
-    @commands.command()
-    async def rename(self, ctx, name):
+    @main.subcommand()
+    async def rename(self, interaction: nextcord.Interaction, name):
         print("Renamed to " + str(name))
         await self.bot.user.edit(nick=name)
 
-    @commands.command()
-    async def status(self, ctx):
-        await ctx.send(f'{self.bot.user.name}\nHealth : {self.health}\nHunger : {self.hunger}\nHappiness : {self.happiness}\nStatus : {self.currstatus} ')
+    @main.subcommand()
+    async def status(self, interaction: nextcord.Interaction):
+        await interaction.send(f'{self.bot.user.name}\nHealth : {self.health}\nHunger : {self.hunger}\nHappiness : {self.happiness}\nStatus : {self.currstatus} ')
 
 def setup(bot):
     bot.add_cog(PetCog(bot))
