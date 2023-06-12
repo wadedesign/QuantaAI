6855550b9106491c9ca822857b92b00dde165216
wadder12 <awade75009@gmail.com>
2023-06-12 04:47:59 +0000

    Jun 11, 2023, 11:47 PM

--- bot.py
+++ bot.py
@@ -142,9 +142,9 @@ async def setuplogger_error(interaction: nextcord.Interaction, error):
         #await interaction.send(content="An error occurred while processing the command.")
 
 
-@bot.event
-async def on_bot_close():
-    await bot.session.close()
+#@bot.event
+#async def on_bot_close():
+    #await bot.session.close()
 
 
 bot.run(TOKEN)
