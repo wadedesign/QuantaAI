07b41c23aa0eb711ed063f14a9ffe091f025f66e
wadder12 <awade75009@gmail.com>
2023-06-12 06:13:52 +0000

    Jun 12, 2023, 1:13 AM

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import nextcord
 from dotenv import load_dotenv
 from nextcord.ext import commands
 from pretty_help import PrettyHelp
-from logger import setup_logger
+from Logs.logger import setup_logger
 
 # added by wade
 
