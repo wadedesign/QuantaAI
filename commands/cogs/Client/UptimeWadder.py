33b57dfbe71009edac6dd5fe1c7c5fe87c19dfb2
wadder12 <awade75009@gmail.com>
2023-06-13 00:12:42 +0000

    Jun 12, 2023, 7:12 PM

--- commands/cogs/Client/UptimeWadder.py
+++ commands/cogs/Client/UptimeWadder.py
@@ -1,11 +1,6 @@
 import nextcord
 from nextcord.ext import commands, tasks
-import asyncio
-import random
-import aiohttp
 import psutil
-
-# ! Could add more sub commands to this
 
 class Uptime(commands.Cog, description="Uptime command"):
     def __init__(self, bot):
@@ -21,7 +16,7 @@ class Uptime(commands.Cog, description="Uptime command"):
 
     @tasks.loop(seconds=1)
     async def uptimeCounter(self):
-        self.ts += 2
+        self.ts += 1
         if self.ts == 60:
             self.ts = 0
             self.tm += 1
@@ -38,14 +33,22 @@ class Uptime(commands.Cog, description="Uptime command"):
 
     @nextcord.slash_command(name="uptime", description="Shows the uptime of the bot")
     async def uptimewadder(self, interaction: nextcord.Interaction):
+        days_emoji = "📅"
+        hours_emoji = "⌛"
+        minutes_emoji = "⏳"
+        seconds_emoji = "⏰"
+        cpu_emoji = "💻"
+        ram_emoji = "🔒"
+        disk_emoji = "💾"
+
         embed = nextcord.Embed(title="Uptime", description="Here is the uptime of the bot", color=nextcord.Color.blue())
-        embed.add_field(name="Days", value=self.td, inline=True)
-        embed.add_field(name="Hours", value=self.th, inline=True)
-        embed.add_field(name="Minutes", value=self.tm, inline=True)
-        embed.add_field(name="Seconds", value=self.ts, inline=True)
-        embed.add_field(name="CPU", value=f"{psutil.cpu_percent()}%", inline=True)
-        embed.add_field(name="RAM", value=f"{psutil.virtual_memory().percent}%", inline=True)
-        embed.add_field(name="Disk", value=f"{psutil.disk_usage('/').percent}%", inline=True)
+        embed.add_field(name=f"{days_emoji} Days", value=self.td, inline=True)
+        embed.add_field(name=f"{hours_emoji} Hours", value=self.th, inline=True)
+        embed.add_field(name=f"{minutes_emoji} Minutes", value=self.tm, inline=True)
+        embed.add_field(name=f"{seconds_emoji} Seconds", value=self.ts, inline=True)
+        embed.add_field(name=f"{cpu_emoji} CPU", value=f"{psutil.cpu_percent()}%", inline=True)
+        embed.add_field(name=f"{ram_emoji} RAM", value=f"{psutil.virtual_memory().percent}%", inline=True)
+        embed.add_field(name=f"{disk_emoji} Disk", value=f"{psutil.disk_usage('/').percent}%", inline=True)
         await interaction.send(embed=embed)
 
 def setup(bot):
