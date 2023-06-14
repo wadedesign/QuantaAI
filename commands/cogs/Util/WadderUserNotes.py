321d1c1a8ba901adb0998d5f5db4d32656530844
wadder12 <awade75009@gmail.com>
2023-06-14 22:22:00 +0000

    Wednesday, June 14, 2023 at 5:22 PM Central Daylight Time

--- commands/cogs/Util/WadderUserNotes.py
+++ commands/cogs/Util/WadderUserNotes.py
@@ -1,40 +1,24 @@
 import nextcord
-import json
 from nextcord.ext import commands
-import os
+import urllib.parse
+from pymongo import MongoClient
 
-
-# ** ready for production
-
-# Set the path for the folder where the user_notes.json file will be saved
-data_folder = "data"
-os.makedirs(data_folder, exist_ok=True)
-
-# Set the file name for storing user notes
-USER_NOTES_FILE = os.path.join(data_folder, 'user_notes.json')
+# MongoDB connection details
+username = urllib.parse.quote_plus("apwade75009")
+password = urllib.parse.quote_plus("Celina@12")
+cluster = MongoClient(f"mongodb+srv://{username}:{password}@quantaai.irlbjcw.mongodb.net/")
+db = cluster["QuantaAI"]
+user_notes_collection = db["user_notes"]
 
 class UserNotes(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.user_notes = self.load_notes()
-
-    def load_notes(self):
-        try:
-            with open(USER_NOTES_FILE, 'r') as f:
-                return json.load(f)
-        except FileNotFoundError:
-            return {}
-
-    def save_notes(self):
-        with open(USER_NOTES_FILE, 'w') as f:
-            json.dump(self.user_notes, f, indent=4)
 
     async def is_admin(self, user: nextcord.User):
         return user.guild_permissions.administrator
 
     @nextcord.slash_command(name="user_memoirs", description="Store and display notes about a specified user")
     async def user_notes(self, interaction: nextcord.Interaction, action: str, user: nextcord.User, note: str = None):
-
         if not await self.is_admin(interaction.user):
             await interaction.response.send_message("You don't have permission to use this command.", ephemeral=True)
             return
@@ -43,8 +27,7 @@ class UserNotes(commands.Cog):
 
         if action == "add":
             if note:
-                self.user_notes.setdefault(user_id, []).append(note)
-                self.save_notes()
+                user_notes_collection.update_one({"user_id": user_id}, {"$push": {"notes": note}}, upsert=True)
                 await interaction.response.send_message(
                     content=f":white_check_mark: Note added for {user.name}: {note}",
                     ephemeral=True
@@ -55,9 +38,9 @@ class UserNotes(commands.Cog):
                     ephemeral=True
                 )
         elif action == "view":
-            notes = self.user_notes.get(user_id, [])
-            if notes:
-                notes_list = "\n".join(f"- {n}" for n in notes)
+            result = user_notes_collection.find_one({"user_id": user_id})
+            if result and "notes" in result:
+                notes_list = "\n".join(f"- {n}" for n in result["notes"])
                 embed = nextcord.Embed(
                     title=f"Notes for {user.name}",
                     description=notes_list,
@@ -71,16 +54,18 @@ class UserNotes(commands.Cog):
                 )
         elif action == "delete":
             if note:
-                notes = self.user_notes.get(user_id, [])
-                if note in notes:
-                    notes.remove(note)
-                    self.save_notes()
-                    await interaction.response.send_message(
-                        content=f":wastebasket: Note deleted for {user.name}: {note}",
-                        ephemeral=True
-                    )
+                result = user_notes_collection.find_one({"user_id": user_id})
+                if result and "notes" in result:
+                    if note in result["notes"]:
+                        user_notes_collection.update_one({"user_id": user_id}, {"$pull": {"notes": note}})
+                        await interaction.response.send_message(
+                            content=f":wastebasket: Note deleted for {user.name}: {note}",
+                            ephemeral=True
+                        )
+                    else:
+                        await interaction.response.send_message("Note not found.", ephemeral=True)
                 else:
-                    await interaction.response.send_message("Note not found.", ephemeral=True)
+                    await interaction.response.send_message("No notes found for the user.", ephemeral=True)
             else:
                 await interaction.response.send_message(
                     content="Please provide a note to delete.",
