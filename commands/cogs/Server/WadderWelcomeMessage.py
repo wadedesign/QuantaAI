e43b936029057d83ea0bf27bddd0ed24d8fc80ca
wadder12 <awade75009@gmail.com>
2023-06-15 00:53:13 +0000

    Wednesday, June 14, 2023 at 7:53 PM Central Daylight Time

--- commands/cogs/Server/WadderWelcomeMessage.py
+++ commands/cogs/Server/WadderWelcomeMessage.py
@@ -31,8 +31,9 @@ class WelcomeMessage(commands.Cog):
         guild = member.guild
         welcome_channel_id = self.settings.get(str(guild.id), {}).get("welcome_channel_id")
         welcome_message = self.settings.get(str(guild.id), {}).get("welcome_message")
+        welcome_enabled = self.settings.get(str(guild.id), {}).get("welcome_enabled", True)
 
-        if welcome_channel_id:
+        if welcome_enabled and welcome_channel_id:
             channel = guild.get_channel(welcome_channel_id)
             if channel:
                 embed = nextcord.Embed(
@@ -45,7 +46,7 @@ class WelcomeMessage(commands.Cog):
                 embed.set_footer(text=f"User ID: {member.id} • Joined at: {member.joined_at.strftime('%Y-%m-%d %H:%M:%S')}")
                 await channel.send(content=f"{member.mention}", embed=embed)
 
-        if welcome_message:
+        if welcome_enabled and welcome_message:
             try:
                 await member.send(welcome_message)
             except nextcord.errors.Forbidden:
@@ -69,7 +70,25 @@ class WelcomeMessage(commands.Cog):
         else:
             await ctx.send("An error occurred while processing this command.")
 
+    @nextcord.slash_command(name="togglewelcome", description="Toggle the welcome system.")
+    @commands.has_permissions(administrator=True)
+    async def togglewelcome(self, interaction: nextcord.Interaction):
+        guild_id = str(interaction.guild.id)
+        welcome_enabled = self.settings.get(guild_id, {}).get("welcome_enabled", True)
+        welcome_enabled = not welcome_enabled
+        self.settings.setdefault(guild_id, {})
+        self.settings[guild_id]["welcome_enabled"] = welcome_enabled
+        self.save_settings()
+        enabled_status = "enabled" if welcome_enabled else "disabled"
+        await interaction.send(f"The welcome system has been {enabled_status}.")
+
+    @togglewelcome.error
+    async def togglewelcome_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send("You do not have permission to use this command.")
+        else:
+            await ctx.send("An error occurred while processing this command.")
+
 
 def setup(bot):
     bot.add_cog(WelcomeMessage(bot))
-
