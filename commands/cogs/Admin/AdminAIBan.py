824c9c1713dd364fba4c9caf6f6e0fb978119b26
wadder12 <awade75009@gmail.com>
2023-06-15 21:03:01 +0000

    Thursday, June 15, 2023 at 4:03 PM Central Daylight Time

--- commands/cogs/Admin/AdminAIBan.py
+++ commands/cogs/Admin/AdminAIBan.py
@@ -1,69 +1,69 @@
 import asyncio
 import os
-import json
 import nextcord
 from nextcord.ext import commands
 import openai
-
-## !Fix not being able to disable the ban hammer
+from pymongo import MongoClient
+import urllib.parse
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# MongoDB connection details
+username = urllib.parse.quote_plus("apwade75009")
+password = urllib.parse.quote_plus("Celina@12")
+cluster = MongoClient(f"mongodb+srv://{username}:{password}@quantaai.irlbjcw.mongodb.net/")
+db = cluster["QuantaAI"]  # Replace "YourNewDatabaseName" with your desired database name
+banhammer_collection = db["banhammer"]
 
 class BanHammer(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.settings_file = "data/ban_hammer_settings.json"
-        if not os.path.isfile(self.settings_file):
-            with open(self.settings_file, "w") as f:
-                json.dump({}, f)
-        self.warns = {}
-
-    def _load_settings(self):
-        with open(self.settings_file, "r") as f:
-            return json.load(f)
-
-    def _save_settings(self, settings):
-        with open(self.settings_file, "w") as f:
-            json.dump(settings, f)
 
     async def is_ban_hammer_enabled(self, guild_id):
-        settings = self._load_settings()
-        return settings.get(str(guild_id), {}).get("enabled", False)
-    
+        result = banhammer_collection.find_one({"_id": guild_id})
+        if result:
+            return result.get("enabled", False)
+        return False
+
+    async def set_ban_hammer_enabled(self, guild_id, enabled):
+        banhammer_collection.update_one(
+            {"_id": guild_id},
+            {"$set": {"enabled": enabled}},
+            upsert=True
+        )
+
     async def warn_user(self, user, guild):
         user_id = str(user.id)
-        if user_id not in self.warns:
-            self.warns[user_id] = 0
-        self.warns[user_id] += 1
+        result = banhammer_collection.find_one({"_id": user_id})
+        warns = result.get("warns", 0) if result else 0
+        warns += 1
 
-        if self.warns[user_id] >= 5:
+        if warns >= 5:
             # Temporarily ban the user for 3 days
             await user.ban(reason="Inappropriate messages detected by Ban Hammer", delete_message_days=0)
             await asyncio.sleep(3 * 24 * 60 * 60)  # Wait for 3 days
             await guild.unban(user)
-            self.warns[user_id] = 0
-            return True
-        return False
+            warns = 0
 
-
+        banhammer_collection.update_one(
+            {"_id": user_id},
+            {"$set": {"warns": warns}},
+            upsert=True
+        )
 
     @nextcord.slash_command(name="banhammer", description="Enable or disable the Ban Hammer")
     @commands.has_permissions(administrator=True)
     async def banhammer(self, interaction: nextcord.Interaction):
-        settings = self._load_settings()
         guild_id = str(interaction.guild.id)
-        enabled = settings.get(guild_id, {}).get("enabled", False)
+        enabled = await self.is_ban_hammer_enabled(guild_id)
 
         if enabled:
-            settings[guild_id]["enabled"] = False
+            await self.set_ban_hammer_enabled(guild_id, False)
             message = "Ban Hammer has been disabled."
         else:
-            if guild_id not in settings:
-                settings[guild_id] = {}
-            settings[guild_id]["enabled"] = True
+            await self.set_ban_hammer_enabled(guild_id, True)
             message = "Ban Hammer has been enabled."
 
-        self._save_settings(settings)
         await interaction.send(message)
 
     @commands.Cog.listener()
@@ -82,12 +82,15 @@ class BanHammer(commands.Cog):
             return
 
         if flagged:
-            banned = await self.warn_user(message.author, message.guild)
-            if banned:
+            await self.warn_user(message.author, message.guild)
+            result = banhammer_collection.find_one({"_id": str(message.author.id)})
+            warns = result.get("warns", 0) if result else 0
+
+            if warns == 5:
                 await message.channel.send(f"{message.author.name} has been temporarily banned for 3 days due to inappropriate messages.")
             else:
                 warning_message = f"{message.author.name}, this is a warning. You have sent an inappropriate message. "\
-                                  f"You have {self.warns[str(message.author.id)]} out of 5 warnings. "\
+                                  f"You have {warns} out of 5 warnings. "\
                                   f"Reaching 5 warnings will result in a temporary ban for 3 days."
                 await message.channel.send(warning_message)
                 try:
