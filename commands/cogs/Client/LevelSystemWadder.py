f40a862f615d5a73e5c39d1fa00fe542f75bb1da
wadder12 <awade75009@gmail.com>
2023-06-15 23:17:11 +0000

    Thursday, June 15, 2023 at 6:17 PM Central Daylight Time

--- commands/cogs/Client/LevelSystemWadder.py
+++ commands/cogs/Client/LevelSystemWadder.py
@@ -2,15 +2,18 @@ import os
 import nextcord
 from nextcord.ext import commands
 import random
-import sqlite3
+from pymongo import MongoClient
+import urllib.parse
 
 level = ['NoobMemer', 'MemeRular', 'MemeStar', 'AlphaMemer']
 levelnum = [5, 10, 15, 20]
 
-db_path = os.path.join("data", "levelling.db")
-conn = sqlite3.connect(db_path)
-c = conn.cursor()
-c.execute('''CREATE TABLE IF NOT EXISTS levelling (user_id INTEGER PRIMARY KEY, username TEXT, server_name TEXT, user_roles TEXT, xp INTEGER, current_xp INTEGER, level INTEGER)''')
+# MongoDB connection details
+username = urllib.parse.quote_plus("apwade75009")
+password = urllib.parse.quote_plus("Celina@12")
+cluster = MongoClient(f"mongodb+srv://{username}:{password}@quantaai.irlbjcw.mongodb.net/")
+db = cluster["QuantaAI"]  # Replace "YourNewDatabaseName" with your desired database name
+levelling_collection = db["levelling"]
 
 def user_level_info(current_xp, lvl):
     if current_xp > lvl * 100:
@@ -21,13 +24,12 @@ def user_level_info(current_xp, lvl):
         return current_xp, lvl
 
 def update_user_roles_info(user_id, role):
-    c.execute("SELECT user_roles FROM levelling WHERE user_id=?", (user_id,))
-    roles_list = c.fetchone()[0].split(',')
+    user = levelling_collection.find_one({"user_id": user_id})
+    roles_list = user.get("user_roles", "").split(",")
     if role not in roles_list:
         roles_list.append(role)
-    roles_str = ','.join(roles_list)
-    c.execute("UPDATE levelling SET user_roles=? WHERE user_id=?", (roles_str, user_id))
-    conn.commit()
+    roles_str = ",".join(roles_list)
+    levelling_collection.update_one({"user_id": user_id}, {"$set": {"user_roles": roles_str}})
 
 def colour_generator():
     r = random.randint(0, 255)
@@ -49,26 +51,34 @@ class LevelSys(commands.Cog):
             return None
 
         if not message.author.bot:
-            c.execute("SELECT * FROM levelling WHERE user_id=?", (message.author.id,))
-            stats = c.fetchone()
-
-            if stats is None:
+            user = levelling_collection.find_one({"user_id": message.author.id})
+            
+            if user is None:
                 role_guild = message.author.guild.roles
                 roles_list = []
                 for role in role_guild:
                     roles_list.append(role.name)
-                roles_str = ','.join(roles_list)
-                newuser = (message.author.id, message.author.name, message.guild.name, roles_str, 0, 0, 1)
-                c.execute("INSERT INTO levelling VALUES (?,?,?,?,?,?,?)", newuser)
-                conn.commit()
+                roles_str = ",".join(roles_list)
+                newuser = {
+                    "user_id": message.author.id,
+                    "username": message.author.name,
+                    "server_name": message.guild.name,
+                    "user_roles": roles_str,
+                    "xp": 0,
+                    "current_xp": 0,
+                    "level": 1
+                }
+                levelling_collection.insert_one(newuser)
             else:
-                current_xp = stats[5]
-                lvl = stats[6]
-                xp = stats[4] + 10
+                current_xp = user["current_xp"]
+                lvl = user["level"]
+                xp = user["xp"] + 10
                 current_xp += 10
                 cur_xp, user_level = user_level_info(current_xp, lvl)
-                c.execute("UPDATE levelling SET current_xp=?, xp=?, level=? WHERE user_id=?", (cur_xp, xp, user_level, message.author.id))
-                conn.commit()
+                levelling_collection.update_one(
+                    {"user_id": message.author.id},
+                    {"$set": {"current_xp": cur_xp, "xp": xp, "level": user_level}}
+                )
                 if lvl >= user_level:
                     pass
                 else:
@@ -93,11 +103,10 @@ class LevelSys(commands.Cog):
         print(f"Checking stats for user ID: {interaction.user.id}")
         
         # Fetch user's stats from the database
-        c.execute("SELECT * FROM levelling WHERE user_id=?", (interaction.user.id,))
-        stats = c.fetchone()
+        user = levelling_collection.find_one({"user_id": interaction.user.id})
     
         # If user has no stats, send a message saying so and return
-        if stats is None:
+        if user is None:
             embed = nextcord.Embed(
                 title="🤔 Your rank stats", 
                 description="You haven't sent any messages, so you have no rank!", 
@@ -107,9 +116,9 @@ class LevelSys(commands.Cog):
             return
     
         # Calculate user's level progress and rank
-        total_xp = stats[4]
-        current_xp = stats[5]
-        lvl = stats[6]
+        total_xp = user["xp"]
+        current_xp = user["current_xp"]
+        lvl = user["level"]
         max_val = (lvl + 1) * 100
         box_ratio = int(max_val / 20)
         green_box = int(current_xp / box_ratio)
@@ -117,10 +126,9 @@ class LevelSys(commands.Cog):
         rank = 1
 
         # Get the rankings from the database and find the user's rank
-        c.execute("SELECT user_id FROM levelling ORDER BY xp DESC")
-        rankings = c.fetchall()
+        rankings = levelling_collection.find().sort("xp", -1)
         for r in rankings:
-            if r[0] == interaction.user.id:
+            if r["user_id"] == interaction.user.id:
                 break
             rank += 1
 
@@ -134,7 +142,7 @@ class LevelSys(commands.Cog):
         embed.add_field(name="Level", value=lvl, inline=True)
         embed.add_field(name="Total XP", value=f'{total_xp}', inline=True)
         embed.add_field(name="XP Progress", value=f"{current_xp}/{max_val}", inline=True)
-        embed.add_field(name="Rank", value=f"{rank}/{len(rankings)}", inline=True)
+        embed.add_field(name="Rank", value=f"{rank}/{rankings.count()}", inline=True)
         embed.add_field(
             name="Progress Bar", 
             value=f"{green_box * ':blue_square:'}{white_box * ':white_large_square:'}", 
@@ -145,16 +153,15 @@ class LevelSys(commands.Cog):
 
     @main.subcommand(name="qleaderboard", description="Check the server's leaderboard")
     async def qleaderboard(self, interaction: nextcord.Interaction):
-        c.execute("SELECT * FROM levelling ORDER BY xp DESC")
-        rankings = c.fetchall()
+        rankings = levelling_collection.find().sort("xp", -1).limit(10)
         i = 1
 
         embed = nextcord.Embed(title="🏆 Leaderboard:", color=colour_generator())
 
         for x in rankings:
             try:
-                temp = interaction.guild.get_member(x[0])
-                tempxp = x[4]
+                temp = interaction.guild.get_member(x["user_id"])
+                tempxp = x["xp"]
                 embed.add_field(
                     name=f"{i}. {temp.display_name}", 
                     value=f"Total XP: {tempxp}",
