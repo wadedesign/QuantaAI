d351a8d6a11bad5f447fca10b1c195cf09589a05
wadder12 <awade75009@gmail.com>
2023-06-15 02:59:59 +0000

    Wednesday, June 14, 2023 at 9:59 PM Central Daylight Time

--- commands/cogs/Server/WadderAntiSpam.py
+++ commands/cogs/Server/WadderAntiSpam.py
@@ -2,9 +2,17 @@ import time
 from collections import defaultdict
 from nextcord import Embed
 from nextcord.ext import commands
+from pymongo import MongoClient
+import urllib.parse
+
+# MongoDB connection details
+username = urllib.parse.quote_plus("apwade75009")
+password = urllib.parse.quote_plus("Celina@12")
+cluster = MongoClient(f"mongodb+srv://{username}:{password}@quantaai.irlbjcw.mongodb.net/")
+db = cluster["QuantaAI"]
+spam_collection = db["spam"]
 
 # ** Ready for Production **
-
 
 class AntiSpam(commands.Cog):
     def __init__(self, bot):
@@ -23,6 +31,7 @@ class AntiSpam(commands.Cog):
 
         current_time = time.time()
         user_id = message.author.id
+        server_id = message.guild.id
 
         # Check if the user has exceeded the message limit
         if current_time - self.user_last_message_time[user_id] < self.time_window:
@@ -30,6 +39,7 @@ class AntiSpam(commands.Cog):
             if self.user_message_count[user_id] > self.message_limit:
                 await message.delete()
                 await message.channel.send(embed=self.create_spam_embed(message.author))
+                self.save_spam_data(message.author.name, message.content, message.guild.name)
         else:
             self.user_message_count[user_id] = 1
         self.user_last_message_time[user_id] = current_time
@@ -40,6 +50,7 @@ class AntiSpam(commands.Cog):
             if self.command_message_count[user_id] > self.command_message_limit:
                 await message.delete()
                 await message.channel.send(embed=self.create_spam_embed(message.author))
+                self.save_spam_data(message.author.name, message.content, message.guild.name)
 
     def create_spam_embed(self, author):
         embed = Embed(title="QAnti-Spam", color=0xff0000)
@@ -47,8 +58,18 @@ class AntiSpam(commands.Cog):
         embed.set_thumbnail(url=author.avatar.url)
         return embed
 
+    def save_spam_data(self, author_name, message_content, server_name):
+        spam_data = {
+            "author_name": author_name,
+            "message_content": message_content,
+            "server_name": server_name,
+            "timestamp": time.time()
+        }
+        spam_collection.insert_one(spam_data)
+
 def setup(bot):
     bot.add_cog(AntiSpam(bot))
+
 
 
 
