38858a4a4110056e274c2150a47323a8c690fe89
wadder12 <awade75009@gmail.com>
2023-06-16 02:18:17 +0000

    Thursday, June 15, 2023 at 9:18 PM Central Daylight Time

--- commands/cogs/Admin/CommandChart.py
+++ commands/cogs/Admin/CommandChart.py
@@ -3,14 +3,8 @@ import typing
 from io import BytesIO
 import nextcord
 from nextcord.ext import commands
-import matplotlib
 import matplotlib.pyplot as plt
-
-matplotlib.use("agg")
-
-
-plt.switch_backend("agg")
-
+import numpy as np
 
 class CommandChart(commands.Cog):
     """Shows the commands most used in a certain channel within the last so-and-so messages"""
@@ -36,55 +30,50 @@ class CommandChart(commands.Cog):
 
     def create_chart(self, top, others, channel):
         plt.clf()
-        sizes = [x[1] for x in top]
-        labels = ["{} {:g}%".format(x[0], x[1]) for x in top]
-        if len(top) >= 20:
-            sizes = sizes + [others]
-            labels = labels + ["Others {:g}%".format(others)]
-        if len(channel.name) >= 19:
-            channel_name = "{}...".format(channel.name[:19])
-        else:
-            channel_name = channel.name
-        title = plt.title("Stats in #{}".format(channel_name), color="white")
+
+        # Set a custom color map
+        cmap = plt.get_cmap("nipy_spectral")
+        colors = cmap(np.linspace(0, 1, len(top)))
+
+        # Set figure and axis properties
+        fig, ax = plt.subplots(figsize=(8, 8), dpi=80)
+        ax.axis("equal")
+
+        # Generate the pie chart
+        wedges, texts, autotexts = ax.pie(
+            [x[1] for x in top] + [others],
+            labels=["{} ({:.1f}%)".format(x[0], x[1]) for x in top] + ["Others ({:.1f}%)".format(others)],
+            colors=colors,
+            autopct="%1.1f%%",
+            startangle=0,
+            wedgeprops=dict(width=0.3),
+            textprops={"color": "w", "fontweight": "bold"},
+        )
+
+        # Set font properties for the text inside the wedges
+        plt.setp(autotexts, size=10, weight="bold")
+
+        # Add a title
+        title = plt.title("Stats in #{}".format(channel.name), color="white", fontsize=16)
         title.set_va("top")
         title.set_ha("center")
-        plt.gca().axis("equal")
-        colors = [
-            "r",
-            "darkorange",
-            "gold",
-            "y",
-            "olivedrab",
-            "green",
-            "darkcyan",
-            "mediumblue",
-            "darkblue",
-            "blueviolet",
-            "indigo",
-            "orchid",
-            "mediumvioletred",
-            "crimson",
-            "chocolate",
-            "yellow",
-            "limegreen",
-            "forestgreen",
-            "dodgerblue",
-            "slateblue",
-            "gray",
-        ]
-        pie = plt.pie(sizes, colors=colors, startangle=0)
-        plt.legend(
-            pie[0],
-            labels,
-            bbox_to_anchor=(0.7, 0.5),
-            loc="center",
-            fontsize=10,
-            bbox_transform=plt.gcf().transFigure,
-            facecolor="#ffffff",
-        )
-        plt.subplots_adjust(left=0.0, bottom=0.1, right=0.45)
+
+        # Add a shadow effect to the wedges
+        for wedge in wedges:
+            wedge.set_edgecolor("k")
+            wedge.set_linewidth(1)
+            wedge.set_alpha(0.8)
+
+        # Remove unnecessary spines and ticks
+        ax.spines["top"].set_visible(False)
+        ax.spines["right"].set_visible(False)
+        ax.spines["bottom"].set_visible(False)
+        ax.spines["left"].set_visible(False)
+        ax.tick_params(axis="both", which="both", bottom=False, top=False, left=False, right=False)
+
+        # Save the chart to an image buffer
         image_object = BytesIO()
-        plt.savefig(image_object, format="PNG", facecolor="#36393E")
+        plt.savefig(image_object, format="PNG", facecolor="#36393E", bbox_inches="tight", pad_inches=0)
         image_object.seek(0)
         return image_object
 
@@ -100,22 +89,21 @@ class CommandChart(commands.Cog):
 
         if not channel:
             channel = interaction.channel
-        if not channel.permissions_for(interaction.user).read_messages == True:
+        if not channel.permissions_for(interaction.user).read_messages:
             await em.delete()
             return await interaction.send("You do not have the proper permissions to access that channel.")
 
         message_list = []
         try:
             async for msg in channel.history(limit=number):
-                # Thanks Sinbad
                 com = await self.command_from_message(msg)
-                if com != None:
+                if com is not None:
                     message_list.append(com.qualified_name)
         except nextcord.errors.Forbidden:
             await em.delete()
             return await interaction.send("I do not have permission to look at that channel.")
-        msg_data = {"total count": 0, "commands": {}}
 
+        msg_data = {"total count": 0, "commands": {}}
         for msg in message_list:
             if len(msg) >= 20:
                 short_name = "{}...".format(msg[:20])
@@ -129,9 +117,10 @@ class CommandChart(commands.Cog):
                 msg_data["commands"][short_name]["count"] = 1
                 msg_data["total count"] += 1
 
-        if msg_data["commands"] == {}:
+        if not msg_data["commands"]:
             await em.delete()
             return await interaction.send("No commands have been run in that channel.")
+
         for command in msg_data["commands"]:
             pd = float(msg_data["commands"][command]["count"]) / float(msg_data["total count"])
             msg_data["commands"][command]["percent"] = round(pd * 100, 1)
@@ -147,6 +136,7 @@ class CommandChart(commands.Cog):
             key=lambda x: x[1],
         )
         others = 100 - sum(x[1] for x in top_ten)
+
         img = self.create_chart(top_ten, others, channel)
         await em.delete()
         await interaction.channel.send(file=nextcord.File(img, "chart.png"))
@@ -154,3 +144,4 @@ class CommandChart(commands.Cog):
 
 def setup(bot):
     bot.add_cog(CommandChart(bot))
+
