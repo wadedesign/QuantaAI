fd892b789585d729911eaeaf938ef96a29f78729
wadder12 <awade75009@gmail.com>
2023-06-18 05:17:31 +0000

    Sunday, June 18, 2023 at 12:17 AM Central Daylight Time

--- bot.py
+++ bot.py
@@ -156,26 +156,7 @@ async def setuplogger(interaction: nextcord.Interaction):
 
 
         
-@bot.command()
-async def get_board(ctx, board_id: int):
-    url = f"https://quantaai.atlassian.net/jira/software/projects/QUAN/boards/{board_id}"
-    headers = {
-        "Accept": "application/json",
-        "Authorization": "Bearer ATATT3xFfGF0Vk_MsWSxVjrEw1CZ4Uhijh_cGJulp1sAsQo6rFCc9058moPqzocG4b9uUI0su2RemGBUUxRWGTaUuXGch3OmiCoBe9D4Ws8PIvn70D3qD4kO1exO5mNMb-pET2X6kdE9NT1Jj1I2V7n0UeBszhUj5ciy72ojKtRKCbOyBCie4qM=867E977F",
-    }
 
-    response = requests.get(url, headers=headers)
-    data = response.json()
-
-    if response.status_code == 200:
-        # Print JSON response to terminal
-        formatted_data = json.dumps(data, sort_keys=True, indent=4, separators=(",", ": "))
-        print(formatted_data)
-
-        # Send JSON response as a message
-        await ctx.send(formatted_data)
-    else:
-        await ctx.send("Failed to retrieve the board.")
 
 
 @bot.slash_command()
