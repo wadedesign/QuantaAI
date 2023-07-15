3aae66e746bdc92b7ddf14b8715dcd25ad2937b0
wadder12 <awade75009@gmail.com>
2023-07-15 01:08:30 +0000

    Friday, July 14, 2023 at 8:08 PM Central Daylight Time

--- commands/cogs/testing/dev.py
+++ commands/cogs/testing/dev.py
@@ -13,21 +13,30 @@ class Developer(commands.Cog):
     async def dev(self, interaction: nextcord.Interaction):
         pass
 
-    @dev.subcommand(name="developer", description="Get info about the bot's developer") 
-
-    async def developer(self, interaction : nextcord.Interaction):
-
-        embed = nextcord.Embed(color=nextcord.Color.blurple())
-        embed.set_author(name="Bot Developer", icon_url=interaction.bot.user.avatar.url)
-        embed.add_field(name="Name", value="Wade5")
-        embed.add_field(name="GitHub", value="[Link to GitHub](https://github.com/Tomato6966/Discord-Js-Handler-Template)")
-        embed.add_field(name="Support Server", value="[Link to Support Server](https://discord.gg/milrato)")
-        embed.set_footer(text="Made with ❤️ using Nextcord")
+    @dev.subcommand(name="developer", description="Get developer info")
+    async def developer(self, interaction: nextcord.Interaction):
+        config_path = os.path.join(os.getcwd(), "botconfig", "config.json")
+        with open(config_path, "r") as config_file:
+            config_data = json.load(config_file)
 
         try:
-            await interaction.send(embed=embed)
+            embed = nextcord.Embed(color=nextcord.Color.blue())  # Replace with the desired color
+            embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/442355791412854784/df7b527a701d9a1ab6d73213576fe295.webp?size=1024")
+            embed.set_author(name="Developer Info", url="https://milrato.eu")
+            embed.add_field(name="🆕 NEW GITHUB", value=f"> There is now an **open Source** Version of this Bot on [Tomato#6966's Github](https://github.com/wadder12)\n> [Link](https://github.com/wadder12/QuantaAI) but please make sure to **give __Credits__** if you use it!\n> Make sure to read the [README](https://github.com/wadder12/QuantaAI) and the [WIKI / FAQ](https://github.com/wadder12/QuantaAI) carefully before opening an [ISSUE](https://github.com/wadder12/QuantaAI)")
+            
+            button = Button(label="Support Server", url="https://discord.com/gg/milrato")
+            view = nextcord.ui.View()
+            view.add_item(button)
+
+            await interaction.response.send_message(embed=embed, ephemeral=True, view=view)
+
         except Exception as e:
-            await interaction.send(f"An error occurred: {e}")
+            print(str(e))
+            error_embed = nextcord.Embed(color=nextcord.Color.red())  # Replace with the desired error color
+            error_embed.title = "Error Occurred"
+            error_embed.description = "An error occurred while executing the command."
+            await interaction.send(embed=error_embed)
     
     @dev.subcommand(name="commands2", description="Get list of available commands")
     async def commands2(self, interaction: nextcord.Interaction):
