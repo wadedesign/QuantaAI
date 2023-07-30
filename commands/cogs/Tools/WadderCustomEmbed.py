db17aa51a9dace84bd6d3bcd3720313cfa026d64
wadder12 <awade75009@gmail.com>
2023-07-30 21:37:50 +0000

    Sunday, July 30, 2023 at 4:37 PM Central Daylight Time

--- commands/cogs/Tools/WadderCustomEmbed.py
+++ commands/cogs/Tools/WadderCustomEmbed.py
@@ -1,70 +1,52 @@
 import nextcord
 from nextcord.ext import commands
 
-class EmbedModal(nextcord.ui.Modal):
+# Ready for Production
+
+class EmbedCus(nextcord.ui.Modal):
 
   def __init__(self):
-
-    super().__init__("Create Embed")
-
-    self.title = nextcord.ui.TextInput(label="Title", min_length=1, max_length=256)
-    self.description = nextcord.ui.TextInput(label="Description", style=nextcord.TextInputStyle.paragraph, min_length=1, max_length=2048)
-
-    self.channel = nextcord.ui.TextInput(label="Channel ID", placeholder="Enter channel ID", min_length=1, max_length=1024)
-    
-    self.author = nextcord.ui.TextInput(label="Author Name", min_length=1, max_length=256)
-
-    self.color = nextcord.ui.Select(
-      options=[
-        nextcord.SelectOption(label="Red", value=0xFF0000),
-        nextcord.SelectOption(label="Green", value=0x00FF00),
-        nextcord.SelectOption(label="Blue", value=0x0000FF)
-      ],
-      placeholder="Select color"
+    super().__init__(
+      "Embed Maker" 
     )
-
-    self.add_item(self.title)
-    self.add_item(self.description)
-    self.add_item(self.channel)  
-    self.add_item(self.author)
+    
+    self.emTitle = nextcord.ui.TextInput(label="Embed Title", min_length=2, max_length=124, required=True, placeholder="Enter the embed title here")
+    self.add_item(self.emTitle)
+    
+    self.emDesc = nextcord.ui.TextInput(label="Embed Description", min_length=5, max_length=2048, required=True, placeholder="Enter the embed description here", style=nextcord.TextInputStyle.paragraph)
+    self.add_item(self.emDesc)
+    
+    self.channelID = nextcord.ui.TextInput(label="Channel ID", required=True, placeholder="Enter the channel ID")
+    self.add_item(self.channelID)
+    
+    self.color = nextcord.ui.TextInput(label="Color", placeholder="Enter hex color code")
     self.add_item(self.color)
 
-  def __dict__(self):
-    dict = super().__dict__()
-    dict['title'] = self.title.value
-    dict['description'] = self.description.value
-    dict['channel'] = self.channel.value
-    dict['author'] = self.author.value
-    dict['color'] = self.color.values[0] if self.color.values else None
-    return dict
-
-  async def callback(self, interaction):
-    embed = nextcord.Embed(
-      title=self.title.value,
-      description=self.description.value,
-      color=int(self.color.values[0], 16) if self.color.values else None
-    )
+  async def callback(self, interaction: nextcord.Interaction) -> None:
+    title = self.emTitle.value
+    desc = self.emDesc.value
+    channel_id = int(self.channelID.value) if self.channelID.value else None
     
-    if self.author.value:
-      embed.set_author(name=self.author.value)
+    em = nextcord.Embed(title=title, description=desc)
     
-    channel_id = self.channel.value if self.channel.value else None
-    if channel_id:
-      channel = interaction.guild.get_channel(int(channel_id))
-      await channel.send(embed=embed) 
-      await interaction.response.send_message("Embed sent!")
+    color = int(self.color.value, 16)
+    em.color = color
+    
+    target_channel = interaction.guild.get_channel(channel_id)
+    if target_channel:
+      await target_channel.send(embed=em)
+      await interaction.response.send_message("Embed sent successfully!")
     else:
-      await interaction.response.send_message("Invalid channel ID")
-  
-class EmbedCog(commands.Cog):
+      await interaction.response.send_message("Invalid channel ID. Please try again.")
+      
+class UserMod(commands.Cog):
 
   def __init__(self, bot):
     self.bot = bot
 
-  @nextcord.slash_command(name="cusembed", description="Create and send embed")
-  async def embed(self, interaction):
-    await interaction.response.send_modal(EmbedModal())
-
+  @nextcord.slash_command(name="cusembed", description="Create an embed")
+  async def embed(self, interaction: nextcord.Interaction):
+    await interaction.response.send_modal(EmbedCus())
+    
 def setup(bot):
-  bot.add_cog(EmbedCog(bot))
-
+  bot.add_cog(UserMod(bot))
