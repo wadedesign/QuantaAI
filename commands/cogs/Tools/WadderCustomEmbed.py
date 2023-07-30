8ac9b0020e2088a171f5d2995f7804034721f06a
wadder12 <awade75009@gmail.com>
2023-07-30 21:19:40 +0000

    Sunday, July 30, 2023 at 4:19 PM Central Daylight Time

--- commands/cogs/Tools/WadderCustomEmbed.py
+++ commands/cogs/Tools/WadderCustomEmbed.py
@@ -7,30 +7,26 @@ class EmbedModal(nextcord.ui.Modal):
 
     super().__init__("Create Embed")
 
-    self.title = nextcord.ui.TextInput(label="Title", min_length=1, max_length=256) 
+    self.title = nextcord.ui.TextInput(label="Title", min_length=1, max_length=256)
     self.description = nextcord.ui.TextInput(label="Description", style=nextcord.TextInputStyle.paragraph)
 
     self.channel = nextcord.ui.TextInput(label="Channel ID", placeholder="Enter channel ID")
 
-    self.footer = nextcord.ui.TextInput(label="Footer Text", required=False)
-    self.footer_image = nextcord.ui.TextInput(label="Footer Image URL", required=False)
     self.author = nextcord.ui.TextInput(label="Author Name", required=False)
 
     self.color = nextcord.ui.Select(
       options=[
         nextcord.SelectOption(label="Red", value=0xFF0000),
         nextcord.SelectOption(label="Green", value=0x00FF00),
-        nextcord.SelectOption(label="Blue", value=0x0000FF)  
+        nextcord.SelectOption(label="Blue", value=0x0000FF)
       ],
       placeholder="Select color"
     )
 
     self.add_item(self.title)
-    self.add_item(self.description) 
+    self.add_item(self.description)
     self.add_item(self.channel)
-    self.add_item(self.footer)
-    self.add_item(self.footer_image)
-    self.add_item(self.author)
+    self.add_item(self.author) 
     self.add_item(self.color)
 
   async def callback(self, interaction):
@@ -39,21 +35,17 @@ class EmbedModal(nextcord.ui.Modal):
     description = self.description.value
     channel = self.channel.value
 
-    footer = self.footer.value
-    footer_image = self.footer_image.value
     author = self.author.value
     color = int(self.color.values[0]) if self.color.values else None
 
     embed = nextcord.Embed(title=title, description=description, color=color)
 
-    if footer:
-      embed.set_footer(text=footer, icon_url=footer_image)
-    if author:  
+    if author:
       embed.set_author(name=author)
 
     channel_id = int(channel) if channel else None
     if channel_id:
-      channel = interaction.guild.get_channel(channel_id) 
+      channel = interaction.guild.get_channel(channel_id)
       await channel.send(embed=embed)
       await interaction.response.send_message("Embed sent!")
     else:
