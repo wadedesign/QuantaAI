0a650b5133411377a9054086cf1f81cfc141884a
wadder12 <awade75009@gmail.com>
2023-07-30 21:13:41 +0000

    Sunday, July 30, 2023 at 4:13 PM Central Daylight Time

--- commands/cogs/Tools/WadderCustomEmbed.py
+++ commands/cogs/Tools/WadderCustomEmbed.py
@@ -1,76 +1,69 @@
 import nextcord
-from nextcord.ext import commands
+from nextcord.ext import commands 
 
-# Ready for Production 
-
-class EmbedCus(nextcord.ui.Modal):
+class EmbedModal(nextcord.ui.Modal):
 
   def __init__(self):
-    super().__init__("Embed Maker")
-    
-    self.emTitle = nextcord.ui.TextInput(label="Embed Title", min_length=2, max_length=124, required=True, placeholder="Enter the embed title here")
-    self.add_item(self.emTitle)
-    
-    self.emDesc = nextcord.ui.TextInput(label="Embed Description", min_length=5, max_length=2048, required=True, placeholder="Enter the embed description here", style=nextcord.TextInputStyle.paragraph)
-    self.add_item(self.emDesc)
-    
-    self.channelID = nextcord.ui.TextInput(label="Channel ID", required=True, placeholder="Enter the channel ID")
-    self.add_item(self.channelID)
 
-    self.color = nextcord.ui.TextInput(label="Color", placeholder="Enter hex color code")
-    self.add_item(self.color)
+    super().__init__("Create Embed") 
     
-    self.footerText = nextcord.ui.TextInput(label="Footer Text", min_length=1, max_length=2048, required=False, placeholder="Enter footer text")
-    self.add_item(self.footerText)
+    self.title = nextcord.ui.TextInput(label="Title", min_length=1, max_length=256)
+    self.description = nextcord.ui.TextInput(label="Description", style=nextcord.TextInputStyle.paragraph)
 
-    self.footerImage = nextcord.ui.TextInput(label="Footer Image URL", required=False, placeholder="Enter URL of footer image")
-    self.add_item(self.footerImage)
+    self.channel = nextcord.ui.TextInput(label="Channel ID", placeholder="Enter channel ID")
 
-    self.authorName = nextcord.ui.TextInput(label="Author Name", min_length=1, max_length=256, required=False, placeholder="Enter author name")
-    self.add_item(self.authorName)
-    
-  async def callback(self, interaction: nextcord.Interaction) -> None:
-    title = self.emTitle.value
-    desc = self.emDesc.value
-    channel_id = int(self.channelID.value) if self.channelID.value else None
-    
-    em = nextcord.Embed(title=title, description=desc)
-    
-    color = int(self.color.value, 16) 
-    em.color = color
-    
-    footer_text = self.footerText.value
-    footer_image = self.footerImage.value
-    author_name = self.authorName.value
-    
-    if footer_text:
-      em.set_footer(text=footer_text)
+    self.footer = nextcord.ui.TextInput(label="Footer Text", required=False)
+    self.footer_image = nextcord.ui.TextInput(label="Footer Image URL", required=False)
+    self.author = nextcord.ui.TextInput(label="Author Name", required=False)
 
-    if footer_image:
-      em.set_footer(icon_url=footer_image)
-      
-    if author_name:
-      em.set_author(name=author_name)
+    self.color = nextcord.ui.Select(
+      options=[
+        nextcord.SelectOption(label="Red", value=0xFF0000),
+        nextcord.SelectOption(label="Green", value=0x00FF00),
+        nextcord.SelectOption(label="Blue", value=0x0000FF)
+      ],
+      placeholder="Select color"  
+    )
 
-    target_channel = interaction.guild.get_channel(channel_id)
-    if target_channel:
-      await target_channel.send(embed=em)
-      await interaction.response.send_message("Embed sent successfully!")
+    self.add_items(self.title, self.description, self.channel, self.footer, self.footer_image, self.author, self.color)
+
+  async def callback(self, interaction):
+    # Get values
+    title = self.title.value
+    description = self.description.value
+    channel = self.channel.value
+    
+    footer = self.footer.value
+    footer_image = self.footer_image.value
+    author = self.author.value
+    color = int(self.color.values[0]) if self.color.values else None
+    
+    # Create embed
+    embed = nextcord.Embed(title=title, description=description, color=color)
+    
+    if footer:
+      embed.set_footer(text=footer, icon_url=footer_image)  
+    if author:
+      embed.set_author(name=author)
+    
+    # Send embed 
+    channel_id = int(channel) if channel else None
+    if channel_id:
+      channel = interaction.guild.get_channel(channel_id)
+      await channel.send(embed=embed)
+      await interaction.response.send_message("Embed sent!")
     else:
-      await interaction.response.send_message("Invalid channel ID. Please try again.")
+      await interaction.response.send_message("Invalid channel ID")
       
-# Rest of cog code
-      
-class UserMod(commands.Cog):
+class EmbedCog(commands.Cog):
 
   def __init__(self, bot):
     self.bot = bot
 
-  @nextcord.slash_command(name="cusembed", description="Create an embed")
-  async def embed(self, interaction: nextcord.Interaction):
-    await interaction.response.send_modal(EmbedCus())
-    
-def setup(bot):
-  bot.add_cog(UserMod(bot))
+  @nextcord.slash_command(name="cusembed", description="Create and send embed")
+  async def embed(self, interaction):
+    await interaction.response.send_modal(EmbedModal())  
 
+def setup(bot):
+  bot.add_cog(EmbedCog(bot))
 
